0b7e5accce3fceff0af40ca3b78915ddd8b0b155
MrEpicx2 <39658357+MrEpicx2@users.noreply.github.com>
2025-10-16 20:25:07 -0600

    foenem button

--- app/src/main.c
+++ app/src/main.c
@@ -7,11 +7,58 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
+#include "BTN.h"
+#include "LED.h"
+
+#define SLEEP_TIME_MS     1
+#define SW0_NODE          DT_ALIAS(sw0)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+static struct gpio_callback button_isr_data;
+
+/*
+void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+  printk("Pressed on foenem 😊\n");
+}*/
 
 int main(void) {
+  /*
+  int ret;
+
+  if (!gpio_is_ready_dt(&button)) {
+    return 0;
+  }
+
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if (0 > ret) {
+    return 0;
+  }
+
+  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if (0 > ret) {
+    return 0;
+  }
+  
+
+  gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
+  gpio_add_callback(button.port, &button_isr_data);
+  */
+
+  if (0 > BTN_init()) {
+    return 0;
+  }
+
+  if (0 > LED_init()) {
+    return 0;
+  }
 
   while(1) {
-
+    if (BTN_check_clear_pressed(BTN0)) {
+      LED_toggle(LED0);
+      printk("Pressed on foenem 😊\n");
+    }
+    k_msleep(SLEEP_TIME_MS);
   }
 	return 0;
 }
